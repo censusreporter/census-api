e25da333d85aaac2352c478319068de5b7c8fe52
Ian Dees <ian.dees@gmail.com>
2013-05-28 21:33:25 -0500

    Give order to the surrounding dict.

--- simple_api.py
+++ simple_api.py
@@ -77,7 +77,10 @@ def geo_summary(geoid):
 
     g.cur.execute("SET search_path=%s", [acs])
 
-    doc = dict(metadata=dict(), population=dict(), geography=dict(), education=dict())
+    doc = OrderedDict([(metadata, dict()),
+                       (population, dict()),
+                       (geography, dict()),
+                       (education, dict())])
 
     doc['metadata']['acs'] = acs
 
