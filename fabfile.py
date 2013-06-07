a77b422e74d915efd917245482c785081ea8491a
Ian Dees <ian.dees@gmail.com>
2013-06-07 12:24:16 -0500

    Modify fabfile to twiddle mod_wsgi bits.

--- fabfile.py
+++ fabfile.py
@@ -6,12 +6,31 @@ from fabric.colors import red
 def deploy(branch='master'):
     "Deploy the specified branch to the remote host."
 
-    code_dir = 'api_app'
+    root_dir = '/home/www-data'
+    code_dir = '%s/api_app' % root_dir
     virtualenv_name = 'api_venv'
+    virtualenv_dir = '%s/%s' % (root_dir, virtualenv_name)
+    host = 'api.censusreporter.org'
+
+    sudo('mkdir -p %s' % root_dir)
+    sudo('chown www-data:www-data %s' % root_dir)
 
     # Install required packages
     sudo('apt-get update')
     sudo('apt-get install -y git libpq-dev python-dev')
+
+    # Install and set up apache and mod_wsgi
+    sudo('apt-get install -y apache2 libapache2-mod-wsgi')
+    sudo('a2enmod wsgi')
+    sudo('rm -f /etc/apache2/sites-enabled/000-default')
+    sudo('rm -f /etc/apache2/sites-enabled/%s' % host)
+    sudo('rm -f /etc/apache2/sites-available/%s' % host)
+    upload_template('./server/apache2/site', '/etc/apache2/sites-available/%s' % host, use_sudo=True, context={
+        'domainname': host,
+        'project_path': '%s/censusreporter' % code_dir,
+        'venv_path': '%s/lib/python2.7/site-packages' % virtualenv_dir
+    })
+    sudo('a2ensite %s' % host)
 
     # Install up to virtualenv
     sudo('apt-get install -y python-setuptools')
@@ -19,19 +38,20 @@ def deploy(branch='master'):
     sudo('pip install virtualenv')
 
     # Create virtualenv and add our django app to its PYTHONPATH
-    run('virtualenv --no-site-packages %s' % virtualenv_name)
+    sudo('virtualenv --no-site-packages %s' % virtualenv_name)
 
     with settings(warn_only=True):
-        if run('test -d %s' % code_dir).failed:
-            print(red('Cloning fresh repo.'))
-            run('git clone git://github.com/censusreporter/census-extractomatic.git %s' % code_dir)
+        if sudo('test -d %s' % code_dir).failed:
+            sudo('git clone git://github.com/censusreporter/census-extractomatic.git %s' % code_dir)
 
     with cd(code_dir):
-        print(red('Pulling %s from GitHub' % branch))
-        run('git pull origin %s' % branch)
+        sudo('git pull origin %s' % branch)
 
         # Install pip requirements
-        run('source /home/ubuntu/%s/bin/activate && pip install -r requirements.txt' % virtualenv_name)
+        sudo('source %s/bin/activate && pip install -r requirements.txt' % virtualenv_name)
 
-        # Run the server
-        run('source /home/ubuntu/%s/bin/activate && python simple_api.py' % virtualenv_name)
+        # Make sure everything is correctly owned
+        sudo('chown www-data:www-data -R %s %s' % (code_dir, virtualenv_dir))
+
+    # Restart apache
+    sudo('service apache2 restart')
