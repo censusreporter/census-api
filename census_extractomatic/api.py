83fdbef2190c424e99c07c22da287b1ffed296d4
Ian Dees <ian.dees@gmail.com>
2013-06-14 12:31:26 -0500

    Add CORS headers to the geo search API for now.

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -3,10 +3,13 @@ from __future__ import division
 
 from flask import Flask
 from flask import abort, request, g
+from flask import make_response, request, current_app
+from functools import update_wrapper
 import json
 import psycopg2
 import psycopg2.extras
 from collections import OrderedDict
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -46,6 +49,48 @@ ACS_NAMES = {
     'acs2007_1yr': {'name': 'ACS 2007 1-year', 'years': '2007'},
     'acs2007_3yr': {'name': 'ACS 2007 3-year', 'years': '2005-2007'},
 }
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
 def sum(data, *columns):
@@ -577,7 +622,9 @@ def table_details(acs, table):
 
     return json.dumps(data)
 
+
 @app.route("/1.0/geo/search")
+@crossdomain(origin='*')
 def geo_search():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
