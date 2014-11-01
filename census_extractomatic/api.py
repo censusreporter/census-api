819298661e6345da375ad454645061f0e625a477
Ian Dees <ian.dees@gmail.com>
2014-11-01 08:19:35 -0400

    Replace 2012 3-year with 2013.

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -45,13 +45,13 @@ if not app.debug:
 # Allowed ACS's in "best" order (newest and smallest range preferred)
 allowed_acs = [
     'acs2013_1yr',
-    'acs2012_3yr',
+    'acs2013_3yr',
     'acs2012_5yr',
 ]
 
 ACS_NAMES = {
     'acs2013_1yr': {'name': 'ACS 2013 1-year', 'years': '2013'},
-    'acs2012_3yr': {'name': 'ACS 2012 3-year', 'years': '2010-2012'},
+    'acs2013_3yr': {'name': 'ACS 2013 3-year', 'years': '2011-2013'},
     'acs2012_5yr': {'name': 'ACS 2012 5-year', 'years': '2008-2012'},
 }
 
