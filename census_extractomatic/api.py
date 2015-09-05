4644e447a95e48eae1b8e95129e57d7f7f7e13e8
Joe Germuska <joe@germuska.com>
2015-09-05 10:57:37 -0500

    pass even more search characters, since otherwise some places are unfindable. censusreporter/censusreporter#133

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -1402,7 +1402,7 @@ def geo_search():
         where = "ST_Intersects(geom, ST_SetSRID(ST_Point(%s, %s),4326))"
         where_args = [lon, lat]
     elif q:
-        q = re.sub(r'[^a-zA-Z\.\-]', ' ', q)
+        q = re.sub(r'[^a-zA-Z\,\.\-]', ' ', q)
         q = re.sub(r'\s+', ' ', q)
         where = "lower(prefix_match_name) LIKE lower(%s)"
         q += '%'
