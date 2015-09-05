ecc34234fdaa9248cb191fd1f6d2de2783670e35
Joe Germuska <joe@germuska.com>
2015-09-05 13:23:37 -0500

    often don't bother with memcache for local dev so comment it out

--- census_extractomatic/config.py
+++ census_extractomatic/config.py
@@ -14,6 +14,6 @@ class Development(Config):
     # For local dev, tunnel to the DB first:
     # ssh -i ~/.ssh/censusreporter.ec2_key.pem -L 5432:localhost:5432 -L 9200:localhost:9200 ubuntu@staging.censusreporter.org
     DATABASE_URI = 'postgresql://census:censuspassword@localhost/census'
-    MEMCACHE_ADDR = ['127.0.0.1']
+    # MEMCACHE_ADDR = ['127.0.0.1']
     ELASTICSEARCH_HOST = ['localhost:9200']
     JSONIFY_PRETTYPRINT_REGULAR = False
