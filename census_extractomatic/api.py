83a3ceb8f0b0e01748ebba1231e5576f07f3bc68
Ian Dees <ian.dees@gmail.com>
2015-09-17 23:41:51 -0400

    Missed a spot I guess?

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -50,7 +50,7 @@ allowed_acs = [
 ]
 
 ACS_NAMES = {
-    'acs2014_1yr': {'name': 'ACS 2013 1-year', 'years': '2013'},
+    'acs2014_1yr': {'name': 'ACS 2014 1-year', 'years': '2014'},
     'acs2013_3yr': {'name': 'ACS 2013 3-year', 'years': '2011-2013'},
     'acs2013_5yr': {'name': 'ACS 2013 5-year', 'years': '2009-2013'},
 }
