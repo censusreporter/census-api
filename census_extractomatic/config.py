ff2e794b25f3cf71ed95e0ae0681b6279e7baa06
Ian Dees <ian.dees@gmail.com>
2016-01-19 23:10:55 -0500

    Use 127.0.0.1 instead of localhost for elasticsearch

--- census_extractomatic/config.py
+++ census_extractomatic/config.py
@@ -5,7 +5,7 @@ class Config(object):
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@censusreporter.c7wefhiuybfb.us-east-1.rds.amazonaws.com:5432/census'
     MEMCACHE_ADDR = ['127.0.0.1']
-    ELASTICSEARCH_HOST = ['localhost:9200']
+    ELASTICSEARCH_HOST = ['127.0.0.1:9200']
     JSONIFY_PRETTYPRINT_REGULAR = False
     MAX_GEOIDS_TO_SHOW = 3500
     MAX_GEOIDS_TO_DOWNLOAD = 3500
@@ -16,5 +16,5 @@ class Development(Config):
     # ssh -i ~/.ssh/censusreporter.ec2_key.pem -L 5432:localhost:5432 -L 9200:localhost:9200 ubuntu@staging.censusreporter.org
     SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost/census'
     MEMCACHE_ADDR = ['127.0.0.1']
-    ELASTICSEARCH_HOST = ['localhost:9200']
+    ELASTICSEARCH_HOST = ['127.0.0.1:9200']
     JSONIFY_PRETTYPRINT_REGULAR = False
