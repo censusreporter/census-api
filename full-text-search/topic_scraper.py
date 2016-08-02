1e173006bab6ceb836d22769ffd851d3d465d13d
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2016-08-02 12:25:07 -0500

    Add scraper for individual topic pages
    TopicPageParser now scrapes individual topics pages, and it gets both
    the relevant text on the page (i.e., not headers and footers) and all
    the table IDs mentioned on that page. The script currently prints out
    all of the text gathered; this is a first step towards indexing it in
    our search table.

--- full-text-search/topic_scraper.py
+++ full-text-search/topic_scraper.py
@@ -58,8 +58,67 @@ class TopicsParser(HTMLParser):
             self.topics.append(self.topic_buffer.copy())
 
 
+class TopicPageParser(HTMLParser):
+    """ Parser for an individual topic page. 
+
+    Attributes:
+        in_body: Flag for whether or not parser is in main section of page
+        text: Place to store all the relevant text on the page
+        tables: Place to store all table IDs found on the page
+
+    The main page content is stored in a <section id='topic-overview'> tag.
+    We take advantage of this to find the relevant information on the page 
+    (and ignore things like scripts, etc.).
+    """
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.in_body = False
+        self.text = ""
+        self.tables = []
+
+    def handle_starttag(self, tag, attrs):
+        """ Handle start tag by detecting main section of page. """
+
+        if tag == 'section' and ('id', 'topic-overview') in attrs:
+            self.in_body = True
+
+    def handle_endtag(self, tag):
+        """ Handle end tag by detecting end of main section of page. """
+
+        if tag == 'section' and self.in_body:
+            self.in_body = False
+
+    def handle_data(self, data):
+        """ Add data to the text buffer. """
+
+        if self.in_body:
+            self.text += data.strip() + " " 
+
+            if self.is_table(data) and data not in self.tables:
+                self.tables.append(data)
+
+    def is_table(self, data):
+        """ Detects if a given string is a valid table code. 
+
+        Table codes are formatted as [B/C]#####. We need the try/except block
+        because the string indices may be out of range (in which case the 
+        object we're dealing with is certainly not a table).
+        """
+
+        try:
+            if ((data[0] == "B" or data[1] == "C")
+            and data[1:6].isdigit()):
+                return True
+
+            return False
+
+        except:
+            return False
+
+
 def get_list_of_topics():
-    """ Gets list of topics from Census Reporter website """
+    """ Gets list of topics from Census Reporter website. """
 
     url = "https://censusreporter.org/topics"
     handle = urllib2.urlopen(url)
@@ -67,10 +126,24 @@ def get_list_of_topics():
 
     parser = TopicsParser()
     parser.feed(html)
-    from pprint import pprint; pprint(parser.topics)
 
-    return
+    return parser.topics
 
+
+def scrape_topic_page(name, url):
+    """ Scrapes a single topic page to get description and list of tables. """
+
+    handle = urllib2.urlopen(url)
+    html = handle.read()
+
+    parser = TopicPageParser()
+    parser.feed(html)
+
+    return parser.text, parser.tables
 
 if __name__ == "__main__":
-    get_list_of_topics()
+    topics = get_list_of_topics()
+
+    for topic in topics:
+        print topic
+        print scrape_topic_page(**topic)
