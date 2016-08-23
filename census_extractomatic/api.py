a48d8e6b72279c8c23f1d75426f610e6305f4c3c
Joe Germuska <joe@germuska.com>
2016-08-23 12:14:05 -0500

    Fix syntax error

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -499,18 +499,19 @@ def get_data_fallback(table_ids, geoids, acs=None):
 
 def special_case_parents(geoid, levels):
     '''
-    Update the parents list for special-cased geographies.
+    Update/adjust the parents list for special-cased geographies.
     '''
     if geoid == '16000US1150000':
         # compare Washington, D.C., to "parent" state of VA,
         # rather than comparing to self as own parent state
+
         target = (index for (index, d) in enumerate(levels) if d['geoid'] == '04000US11').next()
         levels[target].update({
             'coverage': 0,
             'display_name': 'Virginia',
             'geoid': '04000US51'
         })
-        
+
     return levels
 
 def compute_profile_item_levels(geoid):
@@ -578,7 +579,7 @@ def compute_profile_item_levels(geoid):
             'coverage': 100.0,
         })
 
-    special_case_parents(geoid, levels):
+    levels = special_case_parents(geoid, levels)
 
     return levels
 
