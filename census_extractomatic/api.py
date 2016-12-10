7a26aa8eb769ac41f683a9c942a640aafd407aaa
Ian Dees <ian.dees@gmail.com>
2016-12-10 14:10:21 -0500

    Remove 2014 1yr, 2013 3yr, and tiger2013

--- census_extractomatic/api.py
+++ census_extractomatic/api.py
@@ -52,7 +52,6 @@ except Exception, e:
 allowed_acs = [
     'acs2015_1yr',
     'acs2014_5yr',
-    'acs2014_1yr',
 ]
 # When expanding a container geoid shorthand (i.e. 140|05000US12127),
 # use this ACS. It should always be a 5yr release so as to include as
@@ -66,7 +65,6 @@ default_table_search_release = allowed_acs[1]
 allowed_tiger = [
     'tiger2015',
     'tiger2014',
-    'tiger2013',
 ]
 
 allowed_searches = [
@@ -78,9 +76,7 @@ allowed_searches = [
 
 ACS_NAMES = {
     'acs2015_1yr': {'name': 'ACS 2015 1-year', 'years': '2015'},
-    'acs2014_1yr': {'name': 'ACS 2014 1-year', 'years': '2014'},
     'acs2014_5yr': {'name': 'ACS 2014 5-year', 'years': '2010-2014'},
-    'acs2013_3yr': {'name': 'ACS 2013 3-year', 'years': '2011-2013'},
 }
 
 PARENT_CHILD_CONTAINMENT = {
