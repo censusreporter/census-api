86ed585d2b84d77b25392ad94f3aa219ecc2526c
Joon Park <JoonPark2017@u.northwestern.edu>
2017-03-31 12:30:42 -0500

    Try another format
    This format for excel sheet download sizes the column headers depending
    on the indent size of the said column header.

--- census_extractomatic/config.py
+++ census_extractomatic/config.py
@@ -16,6 +16,7 @@ class Production(Config):
 class Development(Config):
     # For local dev, tunnel to the DB first:
     # ssh -i ~/.ssh/censusreporter.ec2_key.pem -L 5432:censusreporter.c7wefhiuybfb.us-east-1.rds.amazonaws.com:5432 ubuntu@52.71.251.119
-    SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost/census'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost/census'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost:5433/census'
     MEMCACHE_ADDR = ['127.0.0.1']
     JSONIFY_PRETTYPRINT_REGULAR = False
