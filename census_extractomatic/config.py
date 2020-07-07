4345723d6afdb6f4d79dc6d6d9cbb843cd62f1ff
Scott Bradley <scottbradley.mail@gmail.com>
2020-07-07 16:23:05 -0500

    Get database and memcached endpoints from environ in development for docker-compose support

--- census_extractomatic/config.py
+++ census_extractomatic/config.py
@@ -19,13 +19,13 @@ class Production(Config):
 class Development(Config):
     # For local dev, tunnel to the DB first:
     # ssh -i ~/.ssh/censusreporter.ec2_key.pem -L 5432:censusreporter.c7wefhiuybfb.us-east-1.rds.amazonaws.com:5432 ubuntu@52.71.251.119
-    SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost:5433/census'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://census:censuspassword@localhost:5433/census')
     # or if using a local database, use this:
     # SQLALCHEMY_DATABASE_URI = 'postgresql://census:censuspassword@localhost/census'
 
     # Maybe change for local dev:
     CENSUS_REPORTER_URL_ROOT = 'http://localhost:8000'
 
-    MEMCACHE_ADDR = ['127.0.0.1']
+    MEMCACHE_ADDR = [os.environ.get('MEMCACHE_ADDR', '127.0.0.1')]
     JSONIFY_PRETTYPRINT_REGULAR = False
     BYPASS_CACHE=True
