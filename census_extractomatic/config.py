c6dc06191e586c0f46efd5b8deed7331d23a9fc9
Ian Dees <ian.dees@gmail.com>
2020-10-11 14:27:04 -0500

    Adjust configuration to use redis cache in production

--- census_extractomatic/config.py
+++ census_extractomatic/config.py
@@ -7,13 +7,14 @@ class Config(object):
     MAX_GEOIDS_TO_DOWNLOAD = 3500
     CENSUS_REPORTER_URL_ROOT = 'https://censusreporter.org'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BYPASS_CACHE=False
+    BYPASS_CACHE = False
+    CACHE_TYPE = os.environ.get('CACHE_TYPE', 'null')
 
 
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://census:censuspassword@censusreporter.c7wefhiuybfb.us-east-1.rds.amazonaws.com:5432/census')
-    MEMCACHE_ADDR = [os.environ.get('MEMCACHE_HOST', '127.0.0.1')]
     JSONIFY_PRETTYPRINT_REGULAR = False
+    CACHE_REDIS_URL = os.environ.get('REDIS_URL')
 
 
 class Development(Config):
